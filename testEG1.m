35e80065bf9f06255a4fa3b8702809c7175a26ef
xiaofangxd <51200226+xiaofangxd@users.noreply.github.com>
2022-05-29 18:54:09 +0800

    Add files via upload

---
+++ testEG1.m
@@ -0,0 +1,20 @@
+clc
+clear %1,2,3,5,6,7,8,10
+for i = 3:5
+    main('-algorithm',@CommunitySparseEA,'-problem',@NREG1,'-evaluation',2e5,'-run',i)
+    main('-algorithm',@CommunitySparseEA,'-problem',@NREG2,'-evaluation',2e5,'-run',i)
+    main('-algorithm',@CommunitySparseEA,'-problem',@NREG3,'-evaluation',2e5,'-run',i)
+    main('-algorithm',@CommunitySparseEA,'-problem',@NREG5,'-evaluation',2e5,'-run',i)
+    main('-algorithm',@CommunitySparseEA,'-problem',@NREG6,'-evaluation',2e5,'-run',i)
+    main('-algorithm',@CommunitySparseEA,'-problem',@NREG7,'-evaluation',2e5,'-run',i)
+    main('-algorithm',@CommunitySparseEA,'-problem',@NREG8,'-evaluation',2e5,'-run',i)
+    main('-algorithm',@CommunitySparseEA,'-problem',@NREG10,'-evaluation',2e5,'-run',i)
+    main('-algorithm',@CommunitySparseEA,'-problem',@NRRN1,'-evaluation',2e5,'-run',i)
+    main('-algorithm',@CommunitySparseEA,'-problem',@NRRN2,'-evaluation',2e5,'-run',i)
+    main('-algorithm',@CommunitySparseEA,'-problem',@NRRN3,'-evaluation',2e5,'-run',i)
+    main('-algorithm',@CommunitySparseEA,'-problem',@NRRN5,'-evaluation',2e5,'-run',i)
+    main('-algorithm',@CommunitySparseEA,'-problem',@NRRN6,'-evaluation',2e5,'-run',i)
+    main('-algorithm',@CommunitySparseEA,'-problem',@NRRN7,'-evaluation',2e5,'-run',i)
+    main('-algorithm',@CommunitySparseEA,'-problem',@NRRN8,'-evaluation',2e5,'-run',i)
+    main('-algorithm',@CommunitySparseEA,'-problem',@NRRN10,'-evaluation',2e5,'-run',i)
+end
